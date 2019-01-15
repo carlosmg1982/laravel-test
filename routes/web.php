5598c24991a2470e3c4addd3ce62e55b720dbc8e
Carlos Martínez <carlosmg72@hotmail.com>
2019-01-16 00:07:18 +0100

    Laracasts

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@
 Route::get('/','PagesController@home');
 Route::get('/contact','PagesController@contact');
 Route::get('/about','PagesController@about');
+Route::get('/projects','ProjectsController@index');
